df2877777453c827749caeaa3bb9d45fd69ea8bb
rosf73 <alswlzhsclq@naver.com>
2024-07-11 15:54:23 +0900

    feat: update design of speech bubble

--- app/src/main/java/com/rosf73/garcani/ui/core/SpeechBubble.kt
+++ app/src/main/java/com/rosf73/garcani/ui/core/SpeechBubble.kt
@@ -1,7 +1,11 @@
 package com.rosf73.garcani.ui.core
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,7 +15,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.unit.dp
+import com.rosf73.garcani.ui.theme.Transparent
+import com.rosf73.garcani.ui.theme.White
 import kotlinx.coroutines.launch
 
 @Composable
@@ -23,27 +30,39 @@ fun SpeechBubble(
     val scrollState = rememberLazyListState()
 
     // TODO : add speech bubble with anim
-    Column(
+    Box(
         modifier = modifier,
     ) {
-        Spacer(modifier = Modifier.weight(1f))
-
-        LazyColumn(
+        Column(
             modifier = Modifier
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            state = scrollState,
+                .align(Alignment.Center)
+                .fillMaxSize(),
         ) {
-            item {
-                Spacer(modifier = Modifier.height(30.dp))
-            }
-            items(textList, key = { it }) { item ->
-                GArcaniText(text = item)
-            }
+            Spacer(modifier = Modifier.weight(1f))
 
-            coroutineScope.launch {
-                scrollState.animateScrollToItem(textList.size)
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                state = scrollState,
+            ) {
+                item {
+                    Spacer(modifier = Modifier.height(30.dp))
+                }
+                items(textList, key = { it }) { item ->
+                    GArcaniText(text = item)
+                }
+
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(textList.size)
+                }
             }
         }
+        Box(
+            modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.verticalGradient(listOf(White, Transparent))),
+        )
     }
 }
