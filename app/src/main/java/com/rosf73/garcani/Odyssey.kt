6f60f8448542bbab96116d91c83be5011ee76d04
rosf73 <alswlzhsclq@naver.com>
2024-07-18 10:12:42 +0900

    feat: update clicked animation

--- app/src/main/java/com/rosf73/garcani/Odyssey.kt
+++ app/src/main/java/com/rosf73/garcani/Odyssey.kt
@@ -1,7 +1,10 @@
 package com.rosf73.garcani
 
-import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.keyframes
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,11 +15,14 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
+import com.rosf73.garcani.ui.anim.animateFloatAsState
 import com.rosf73.garcani.ui.core.GradientButton
 import com.rosf73.garcani.ui.theme.White
 import kotlinx.coroutines.delay
@@ -25,21 +31,12 @@ import kotlinx.coroutines.delay
 fun Odyssey(
     modifier: Modifier = Modifier,
 ) {
-    val offsetY1 = remember { Animatable(-5f) }
-    val offsetY2 = remember { Animatable(-5f) }
-    LaunchedEffect(key1 = offsetY1) {
-        offsetY1.animateTo(
-            0.5f,
-            animationSpec = TweenSpec(2000),
-        )
-    }
-    LaunchedEffect(key1 = offsetY2) {
-        delay(200)
-        offsetY2.animateTo(
-            0.5f,
-            animationSpec = TweenSpec(2000),
-        )
-    }
+    val init1 = remember { mutableFloatStateOf(-5f) }
+    val init2 = remember { mutableFloatStateOf(-5f) }
+    val target1 = remember { mutableFloatStateOf(0.5f) }
+    val target2 = remember { mutableFloatStateOf(0.5f) }
+    var isLeftFast = true
+    val isClicked = remember { mutableStateOf(false) }
 
     Row(
         modifier = modifier
@@ -48,7 +45,30 @@ fun Odyssey(
     ) {
         OdysseyCard(
             modifier = Modifier.weight(1f),
-            offset = offsetY1.value,
+            initOffset = init1.floatValue,
+            targetOffset = target1.floatValue,
+            duration = if (isClicked.value) 600 else 2000,
+            doBefore = { if (!isLeftFast) delay(200) },
+            onClick = {
+                // 위로 살짝
+                init1.floatValue = 0.5f
+                target1.floatValue = 0.35f
+                init2.floatValue = 0.5f
+                target2.floatValue = 0.35f
+
+                isLeftFast = true
+                isClicked.value = true
+            },
+            doAfter = if (isClicked.value) {{
+                // 아래로 사라지기
+                init1.floatValue = 0.35f
+                target1.floatValue = 1.5f
+                init2.floatValue = 0.35f
+                target2.floatValue = 1.5f
+
+                isLeftFast = true
+                isClicked.value = true
+            }} else {{}}
         ) {
             Text(text = "Thought Card")
         }
@@ -57,7 +77,30 @@ fun Odyssey(
 
         OdysseyCard(
             modifier = Modifier.weight(1f),
-            offset = offsetY2.value,
+            initOffset = init2.floatValue,
+            targetOffset = target2.floatValue,
+            duration = if (isClicked.value) 600 else 2000,
+            doBefore = { if (isLeftFast) delay(200) },
+            onClick = {
+                // 위로 살짝
+                init1.floatValue = 0.5f
+                target1.floatValue = 0.35f
+                init2.floatValue = 0.5f
+                target2.floatValue = 0.35f
+
+                isLeftFast = false
+                isClicked.value = true
+            },
+            doAfter = if (isClicked.value) {{
+                // 아래로 사라지기
+                init1.floatValue = 0.35f
+                target1.floatValue = 1.5f
+                init2.floatValue = 0.35f
+                target2.floatValue = 1.5f
+
+                isLeftFast = true
+                isClicked.value = true
+            }} else {{}}
         ) {
             Text(text = "Tarot")
         }
@@ -67,9 +110,24 @@ fun Odyssey(
 @Composable
 private fun OdysseyCard(
     modifier: Modifier = Modifier,
-    offset: Float,
+    initOffset: Float,
+    targetOffset: Float,
+    easing: Easing = FastOutSlowInEasing,
+    duration: Int = 2000,
+    doBefore: suspend () -> Unit = {},
+    doAfter: suspend () -> Unit = {},
+    onClick: () -> Unit,
     content: @Composable () -> Unit,
 ) {
+    val offset by animateFloatAsState(
+        initOffset,
+        targetOffset,
+        easing,
+        duration,
+        doBefore = doBefore,
+        doAfter = doAfter,
+    )
+
     GradientButton(
         modifier = modifier
             .graphicsLayer {
@@ -82,7 +140,7 @@ private fun OdysseyCard(
             ),
         elevation = ButtonDefaults.elevatedButtonElevation(),
         shape = MaterialTheme.shapes.medium,
-        onClick = {},
+        onClick = onClick,
     ) {
         content()
     }
