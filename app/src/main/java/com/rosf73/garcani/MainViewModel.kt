ba2140e40f2441226e8de253ee2e4e8acd499394
rosf73 <alswlzhsclq@naver.com>
2024-07-31 17:53:28 +0900

    feat: set SAFETY settings of model

--- app/src/main/java/com/rosf73/garcani/MainViewModel.kt
+++ app/src/main/java/com/rosf73/garcani/MainViewModel.kt
@@ -2,6 +2,9 @@ package com.rosf73.garcani
 
 import androidx.lifecycle.ViewModel
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.BlockThreshold
+import com.google.ai.client.generativeai.type.HarmCategory
+import com.google.ai.client.generativeai.type.SafetySetting
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -17,6 +20,12 @@ class MainViewModel : ViewModel() {
         generativeModel = GenerativeModel(
             modelName = "gemini-1.5-flash",
             apiKey,
+            safetySettings = listOf(
+                SafetySetting(HarmCategory.HATE_SPEECH, BlockThreshold.LOW_AND_ABOVE),
+                SafetySetting(HarmCategory.HARASSMENT, BlockThreshold.LOW_AND_ABOVE),
+                SafetySetting(HarmCategory.DANGEROUS_CONTENT, BlockThreshold.LOW_AND_ABOVE),
+                SafetySetting(HarmCategory.SEXUALLY_EXPLICIT, BlockThreshold.LOW_AND_ABOVE),
+            )
         )
     }
 
