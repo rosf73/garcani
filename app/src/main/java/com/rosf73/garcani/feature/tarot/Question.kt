92eafdb6b12361c523e3c2d5f6e87bcba149ea24
rosf73 <alswlzhsclq@naver.com>
2024-08-08 17:38:31 +0900

    feat: add initFocus to Question

--- app/src/main/java/com/rosf73/garcani/feature/tarot/Question.kt
+++ app/src/main/java/com/rosf73/garcani/feature/tarot/Question.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
@@ -53,9 +56,14 @@ private fun QuestionField(
     onDone: () -> Unit,
 ) {
     val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
     TextField(
-        modifier = modifier,
+        modifier = modifier.focusRequester(focusRequester),
         value = text,
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
         keyboardActions = KeyboardActions(
